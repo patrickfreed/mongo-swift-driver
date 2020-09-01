665701f54072b6f81fcdda39aeaf1f30330bf33a
Patrick Freed <patrick.freed@mongodb.com>
2020-08-31 20:52:20 -0400

    wip fix other tests

--- Sources/TestsCommon/SpecTestUtils.swift
+++ Sources/TestsCommon/SpecTestUtils.swift
@@ -54,7 +54,9 @@ public func retrieveSpecTestFiles<T: Decodable>(
             }
             let url = URL(fileURLWithPath: "\(path)/\(filename)")
             let jsonString = try String(contentsOf: url, encoding: .utf8)
-            return try (filename, ExtendedJSONDecoder().decode(T.self, from: jsonString.data(using: .utf8)!))
+            var doc = try ExtendedJSONDecoder().decode(BSONDocument.self, from: jsonString.data(using: .utf8)!)
+            doc["name"] = .string(filename)
+            return try (filename, BSONDecoder().decode(T.self, from: doc))
         }
 }
 
