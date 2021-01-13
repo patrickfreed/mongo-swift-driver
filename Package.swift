7fd9e84fa6dc2e89001c718281851610ff7a21c0
Patrick Freed <patrick.freed@mongodb.com>
2021-01-12 20:45:14 -0500

    use local

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/Quick/Nimble.git", .upToNextMajor(from: "8.0.0")),
         .package(url: "https://github.com/apple/swift-nio", .upToNextMajor(from: "2.15.0")),
-        .package(url: "https://github.com/patrickfreed/swift-bson.git", .branch("SWIFT-1026/json-to-bson-rebased"))
+        .package(path: "/home/patrick/swift-bson")
     ],
     targets: [
         .target(name: "MongoSwift", dependencies: ["CLibMongoC", "NIO", "NIOConcurrencyHelpers", "SwiftBSON",]),
