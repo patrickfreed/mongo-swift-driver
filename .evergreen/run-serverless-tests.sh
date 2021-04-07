7063f884200df2bec9dd031132f00b8ebade3005
Patrick Freed <patrick.freed@mongodb.com>
2021-04-07 18:58:36 -0400

    disable xtrace

--- .evergreen/run-serverless-tests.sh
+++ .evergreen/run-serverless-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 set -o errexit  # Exit the script with error if any of the commands fail
+set +o xtrace   # disable xtrace to avoid leaking credentials
 
 # variables
 PROJECT_DIRECTORY=${PROJECT_DIRECTORY:-$PWD}
